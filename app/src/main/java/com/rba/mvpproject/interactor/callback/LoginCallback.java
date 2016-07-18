f754324aa64c81a116ff212d105184f7443e32da
Ricardo Bravo <ricardobravo@outlook.com>
2016-07-17 19:44:08 -0500

    feat(loginPresenter): add loginPresenter

--- app/src/main/java/com/rba/mvpproject/interactor/callback/LoginCallback.java
+++ app/src/main/java/com/rba/mvpproject/interactor/callback/LoginCallback.java
@@ -8,5 +8,4 @@ public interface LoginCallback {
 
     void onSuccess(Object object);
     void onError(Object object);
-
 }
--- app/src/main/java/com/rba/mvpproject/presenter/LoginPresenter.java
+++ app/src/main/java/com/rba/mvpproject/presenter/LoginPresenter.java
@@ -1,8 +1,13 @@
 package com.rba.mvpproject.presenter;
 
+import android.util.Log;
+
 import com.rba.mvpproject.interactor.LogInInteractor;
 import com.rba.mvpproject.interactor.callback.LoginCallback;
+import com.rba.mvpproject.model.response.UserResponse;
 import com.rba.mvpproject.ui.view.LoginView;
+
+import java.util.HashMap;
 
 /**
  * Created by Ricardo Bravo on 17/07/16.
@@ -11,20 +16,27 @@ import com.rba.mvpproject.ui.view.LoginView;
 public class LoginPresenter implements LoginCallback {
 
     LoginView loginView;
-    LogInInteractor logInInteractor;
 
-    public LoginPresenter(LoginView loginView, LogInInteractor logInInteractor) {
+    public LoginPresenter(LoginView loginView) {
         this.loginView = loginView;
-        this.logInInteractor = logInInteractor;
+    }
+
+    public void login(HashMap<String, String> data) {
+        loginView.showLoading();
+        LogInInteractor.login(data, this);
     }
 
     @Override
     public void onSuccess(Object object) {
-
+        UserResponse userResponse = (UserResponse) object;
+        loginView.hideLoading();
+        loginView.next(userResponse);
     }
 
     @Override
     public void onError(Object object) {
-
+        String value = (String) object;
+        Log.i("x- onError", value);
+        loginView.hideLoading();
     }
 }
